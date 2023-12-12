92e9e149cbf3286ef8155982ba38132b6df66c96
Ândriu <andriu_coelho1993@hotmail.com>
2023-12-12 14:24:58 -0300

    aula 2

--- Cinetopia/Views/Movies/MoviesView.swift
+++ Cinetopia/Views/Movies/MoviesView.swift
@@ -1,18 +1,29 @@
 //
-//  MoviesViewController.swift
+//  MoviesView.swift
 //  Cinetopia
 //
-//  Created by Giovanna Moeller on 30/10/23.
+//  Created by ALURA on 01/12/23.
 //
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+protocol MoviesViewProtocol: AnyObject {
+    func setupView(with movies: [Movie])
+    func reloadData()
+    func navigateToMovieDetails(movie: Movie)
+    func reloadRow(at indexPath: IndexPath)
+    func toggle(_ isActive: Bool)
+}
+
+class MoviesView: UIView {
+    
+    // MARK: - Attributes
     
     private var filteredMovies: [Movie] = []
     private var isSearchActive: Bool = false
-    private var movieService: MovieService = MovieService()
     private var movies: [Movie] = []
+
+    // MARK: - UI Components
     
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
@@ -24,74 +35,45 @@ class MoviesViewController: UIViewController {
         return tableView
     }()
     
-    private lazy var searchBar: UISearchBar = {
+    private(set) lazy var searchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.placeholder = "Pesquisar"
         searchBar.searchTextField.backgroundColor = .white
-        searchBar.delegate = self
+//        searchBar.delegate = self
         return searchBar
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .background
-        setupNavigationBar()
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .background
         addSubviews()
         setupConstraints()
-        Task {
-            await fetchMovies()
-        }
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        tableView.reloadData()
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
-    private func fetchMovies() async {
-        do {
-            movies = try await movieService.getMovies()
-            tableView.reloadData()
-        } catch (let error) {
-            print(error)
-        }
-    }
+    // MARK: - Class methods
     
     private func addSubviews() {
-        view.addSubview(tableView)
+        addSubview(tableView)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
         ])
     }
     
-    private func setupNavigationBar() {
-        title = "Filmes Populares"
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        navigationItem.setHidesBackButton(true, animated: true)
-        navigationItem.titleView = searchBar
-    }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
 
-extension MoviesViewController: UITableViewDataSource, UITableViewDelegate {
+extension MoviesView: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return isSearchActive ? filteredMovies.count : movies.count
     }
@@ -111,7 +93,7 @@ extension MoviesViewController: UITableViewDataSource, UITableViewDelegate {
         tableView.deselectRow(at: indexPath, animated: true)
         let movie = isSearchActive ? filteredMovies[indexPath.row] : movies[indexPath.row]
         let detailsVC = MovieDetailsViewController(movie: movie)
-        navigationController?.pushViewController(detailsVC, animated: true)
+//        navigationController?.pushViewController(detailsVC, animated: true)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -119,21 +101,7 @@ extension MoviesViewController: UITableViewDataSource, UITableViewDelegate {
     }
 }
 
-extension MoviesViewController: UISearchBarDelegate {
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if searchText.isEmpty {
-            isSearchActive = false
-        } else {
-            filteredMovies = movies.filter { movie in
-                movie.title.lowercased().contains(searchText.lowercased())
-            }
-            isSearchActive = true
-        }
-        tableView.reloadData()
-    }
-}
-
-extension MoviesViewController: MovieTableViewCellDelegate {
+extension MoviesView: MovieTableViewCellDelegate {
     func didSelectFavoriteButton(sender: UIButton) {
         guard let cell = sender.superview?.superview as? MovieTableViewCell else {
             return
@@ -148,5 +116,27 @@ extension MoviesViewController: MovieTableViewCellDelegate {
         
         MovieManager.shared.add(selectedMovie)
         tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+}
+
+extension MoviesView: MoviesViewProtocol {
+    func setupView(with movies: [Movie]) {
+        self.movies = movies
+    }
+    
+    func reloadData() {
+        tableView.reloadData()
+    }
+    
+    func navigateToMovieDetails(movie: Movie) {
+        
+    }
+    
+    func reloadRow(at indexPath: IndexPath) {
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+    
+    func toggle(_ isActive: Bool) {
+        self.isSearchActive = isActive
     }
 }
---
+++ Cinetopia/Views/Movies/MoviesViewController.swift
@@ -0,0 +1,67 @@
+//
+//  MoviesViewController.swift
+//  Cinetopia
+//
+//  Created by Giovanna Moeller on 30/10/23.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+    
+    private var movieService: MovieService = MovieService()
+    
+    private lazy var mainView: MoviesView = {
+        let mainView = MoviesView()
+        
+        return mainView
+    }()
+    
+    override func loadView() {
+        view = mainView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+        Task {
+            await fetchMovies()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tableView.reloadData()
+    }
+    
+    private func fetchMovies() async {
+        do {
+            let movies = try await movieService.getMovies()
+            tableView.reloadData()
+        } catch (let error) {
+            print(error)
+        }
+    }
+    
+    private func setupNavigationBar() {
+        title = "Filmes Populares"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationItem.setHidesBackButton(true, animated: true)
+        navigationItem.titleView = mainView.searchBar
+    }
+}
+
+extension MoviesViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        if searchText.isEmpty {
+//            isSearchActive = false
+//        } else {
+//            filteredMovies = movies.filter { movie in
+//                movie.title.lowercased().contains(searchText.lowercased())
+//            }
+//            isSearchActive = true
+//        }
+//        tableView.reloadData()
+    }
+}
